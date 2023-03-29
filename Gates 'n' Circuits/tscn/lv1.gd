d3b14c1147428361f487a010b994c0df774876e8
PeaPodsYuu <108401575+PeaPodsYuu@users.noreply.github.com>
2023-03-29 20:07:35 +0300

    0.2

--- Gates 'n' Circuits/tscn/lv1.gd
+++ Gates 'n' Circuits/tscn/lv1.gd
@@ -10,6 +10,18 @@ func _ready():
 	arrange_items()
 	pass # Replace with function body.
 
+func add_cable_vert(pos):
+	var cable = preload("res://data/objects/Cable.tscn").instance()
+	cable.scale = Vector2(0.5,0.5)
+	cable.position = pos
+	add_child(cable)
+
+func add_cable2_hor(pos):
+	var cable = preload("res://data/objects/Cable.tscn").instance()
+	cable.rotate(PI/2)
+	cable.scale = Vector2(0.5,0.5)
+	cable.position = pos
+	add_child(cable)
 
 func arrange_items():
 	var button = preload("res://data/objects/button.tscn").instance()
@@ -18,7 +30,7 @@ func arrange_items():
 	button.name = "button"
 	add_child(button)
 	
-	var cable = preload("res://data/objects/Cable.tscn").instance()
-	cable.scale = Vector2(0.5,0.5)
-	cable.position = Vector2(128,128)*4+Vector2(0,64)
-	add_child(cable)
+	add_cable_vert(Vector2(128,128)*4 + Vector2(0,64))
+	add_cable_vert(Vector2(128,128)*4 + Vector2(0,128))
+	add_cable_vert(Vector2(128,128)*4 + Vector2(0,-64))
+	add_cable2_hor(Vector2(128,128)*4 + Vector2(0,-64))
